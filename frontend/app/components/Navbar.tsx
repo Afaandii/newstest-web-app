b2d8c87dfee003157271b2c226506e3a1a1254bb
Afaandii <fandisantuy140@gmail.com>
2026-02-20 19:29:29 +0700

    fixx error frontend

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit quae
+        officia sunt quidem sed doloribus quibusdam culpa magnam eligendi nemo
+        qui repudiandae recusandae eveniet, eos accusamus itaque laborum ad
+        soluta.
+      </p>
+    </nav>
+  );
+}
