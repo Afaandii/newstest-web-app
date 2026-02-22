1d8de573cb73fe28d2358b433c84ddd93b4c6e9f
Afaandii <fandisantuy140@gmail.com>
2026-02-22 18:49:18 +0700

    crud data roles

--- backend/internal/role/routes.go
+++ backend/internal/role/routes.go
@@ -1,1 +1,21 @@
 package role
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func RegisterRoutesRole(r *gin.Engine, db *gorm.DB) {
+	repo := NewRoleRepository(db)
+	service := NewServiceRole(repo)
+	handler := NewHandlerRole(service)
+
+	group := r.Group("/v1/roles")
+	{
+		group.GET("/", handler.GetAll)
+		group.GET("/:id", handler.GetByID)
+		group.POST("/", handler.Create)
+		group.PUT("/:id", handler.Update)
+		group.DELETE("/:id", handler.Delete)
+	}
+}
